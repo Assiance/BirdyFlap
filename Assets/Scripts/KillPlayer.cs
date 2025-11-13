c47febe9d89d32aee5d3db78e29319c5b1104141
Bradford Hill <bradford.foxworth-hill>
2025-11-13 01:51:03 -0500

    add player death

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the colliding object is the player
+        if (other.CompareTag("Player"))
+        {
+            RestartScene();
+        }
+    }
+
+    private void RestartScene()
+    {
+        // Reload the current active scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
