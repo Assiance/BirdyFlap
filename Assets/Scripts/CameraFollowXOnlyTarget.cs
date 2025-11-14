cab04de394ab5558dbe2b5cfcdfe97babd41910b
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2025-11-14 02:25:55 -0500

    added 2d side scrolling

---
+++ Assets/Scripts/CameraFollowXOnlyTarget.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollowXOnlyTarget : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float fixedY = 0f;
+    [SerializeField] private float fixedZ = 0f;
+
+    private void LateUpdate()
+    {
+        if (player == null) return;
+
+        transform.position = new Vector3(
+            player.position.x,
+            fixedY,
+            fixedZ
+        );
+    }
+}
+
